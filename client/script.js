3f3518f5e6080b29660a7d748aeb3d3088f6a07e
Adrian Fernandez <iancroft68@gmail.com>
2023-05-15 16:31:01 +0800

    update the server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from the server, to get the response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://buffalogpt.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
